6ce79ec0efcdd948462faebc55279085ae2ed96a
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-03-11 19:18:19 +0530

    navbar-animation

--- src/portfolioComponents/Home/Navbar.js
+++ src/portfolioComponents/Home/Navbar.js
@@ -1,36 +1,44 @@
-import React from "react";
-import "./Navbar.css";
+import React from 'react';
+import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div className="nav-bar-container">
       <input type="checkbox" id="isChecked" />
       <label for="isChecked" className="menu-button">
-        <i class="fa-solid fa-bars"></i>
-      </label>
+        <i class="fa-solid fa-bars"> </i>{' '}
+      </label>{' '}
       <label for="isChecked" className="menu-button1">
-        <i class="fa-solid fa-xmark"></i>
-      </label>
+        <i class="fa-solid fa-xmark"> </i>{' '}
+      </label>{' '}
       <div class="logo">
-        PORT<span className="color-text">FOLIO</span>
-      </div>
+        <span className={{ '--i': '1' }}>K</span>
+        <span className={{ '--i': '2' }}>A</span>
+        <span className={{ '--i': '3' }}>V</span>
+        <span className={{ '--i': '4' }}>I</span>
+        <span className={{ '--i': '5' }}>N</span>
+        <span className={{ '--i': '6' }}>&nbsp;</span>
+        <span className={{ '--i': '7' }}>
+          <div className="color-text"> P </div>
+        </span>
+      </div>{' '}
       <ul className="nav-links">
         <li>
-          <a href="#home">Home</a>
-        </li>
+          <a href="#home"> Home </a>{' '}
+        </li>{' '}
         <li>
-          <a href="#aboutme">About Me</a>
-        </li>
+          <a href="#aboutme"> About Me </a>{' '}
+        </li>{' '}
         <li>
-          <a href="#resume">Resume</a>
-        </li>
+          <a href="#resume"> Resume </a>{' '}
+        </li>{' '}
         <li>
-          <a href="#achievements">Projects Demo</a>
-        </li>
+          <a href="#achievements"> Projects Demo </a>{' '}
+        </li>{' '}
         <li>
-          <a href="#contactme">Contact Me</a>
-        </li>
-      </ul>
+          <a href="#contactme"> Contact Me </a>{' '}
+        </li>{' '}
+      </ul>{' '}
     </div>
   );
 }
